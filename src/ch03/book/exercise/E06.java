4bad1e46af5eac7a9f85f6c328ad14f0c0473d9e
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2022-08-18 17:46:44 +0900

    20220818 자바오후수업

---
+++ src/ch03/book/exercise/E06.java
@@ -0,0 +1,16 @@
+package ch03.book.exercise;
+
+public class E06 {
+	public static void main(String[] args) {
+		int lengthTop = 5;
+		int lengthBottom = 10;
+		int height = 7;
+		double area = (((lengthTop+lengthBottom)*((double)height))/2);
+		System.out.println(area);
+		
+		
+		byte a = 1;
+		int b = ~a;
+		System.out.println(b);
+	}
+}
