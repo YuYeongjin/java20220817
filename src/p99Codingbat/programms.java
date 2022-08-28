fb53f3e9f2c9512f1421fd4fe5e9574bfcb4fbdd
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2022-08-28 16:09:24 +0900

    20220828 프로그래머스 공부

--- src/p99Codingbat/programms.java
+++ src/p99Codingbat/programms.java
@@ -1,15 +1,36 @@
 package p99Codingbat;
 
+import java.util.Arrays;
+
 public class programms {
 	public static void main(String[] args) {
-	
-		String a = "Alphabet";
-		System.out.println(a.length());
-		for (int i = 0; i<a.length();i++) {
-	    System.out.println(a.charAt(i));
-		}
-	    
-	    
-	    
+		long n = 10000000000L;
+//		long n = 9999999999L;
+		
+		int[] answer = {};
+        int[] a = new int[11];
+        long b= n;
+        int count=0;
+        
+        System.out.println(b);
+        for(int i =0; i<a.length; i++){
+            a[i]=(int)(b%10);
+            b=b/10;            
+        }   
+        System.out.println(Arrays.toString(a));
+        for(int i = 11; i>0; i--) {
+        	System.out.println(a[i-1]);
+        	if (a[i-1]!=0){
+        		count = i;
+        		break;
+        	}
+        }
+        System.out.println(count);
+        answer = new int[count];
+        for(int i =0; i<answer.length; i++){
+            answer[i]=(int)(n%10);
+            n=n/10;            
+        } 
+	    System.out.println(Arrays.toString(answer));
 	}
 }        
