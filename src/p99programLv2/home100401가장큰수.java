23513aedde0ec8c8101df41a4aec63db0c2f3750
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2022-10-10 17:51:55 +0900

    20221010프로그래머스 공부

--- src/p99programLv2/home100401가장큰수.java
+++ src/p99programLv2/home100401가장큰수.java
@@ -3,7 +3,7 @@ package p99programLv2;
 import java.util.*;
 public class home100401가장큰수 {
 	String answer="";
-	Set<String> set = new HashSet<>();
+	TreeSet<String> set = new TreeSet<>((a,b)->b.compareTo(a));
 	public static void main(String[] args) {
 		home100401가장큰수 go = new home100401가장큰수();
 		int[] numbers = {3, 319,30, 34, 380,32,300,309,39, 54,5, 9,91,330};
@@ -11,12 +11,12 @@ public class home100401가장큰수 {
 		String[] num = new String[numbers.length];
 		for(int i = 0 ; i < numbers.length;i++) {
 			num[i]= ""+numbers[i];
+			go.set.add(""+numbers[i]);
 		}
-		
+		System.err.println(go.set);
 		Arrays.parallelSort(num, (a,b)->b.compareTo(a));
 		System.out.println(Arrays.toString(num));
 		for(int i = 9; i >=1;i--) {
-			boolean[] check = new boolean[num.length];
 			List<String> n = new ArrayList<>();
 			int len = 0;
 			
@@ -25,34 +25,44 @@ public class home100401가장큰수 {
 					n.add(num[l]);
 				}
 			}
-			n.sort((a,b)->a.compareTo(b));
 			System.out.println(n);
+	
+			boolean[] check = new boolean[n.size()];
+			
+			
+			
 			for(int j=0;j<n.size();j++) {
 				len+=n.get(j).length();
 			}
 			if(len!=0) {
-				go.dfs(0,"",len,n);	
-			}			
+				go.dfs("",len,n,check);	
+			}
+			System.out.println(go.set);
 		}
+
+
 		
-//		go.bfs(0,num);
-		System.out.println(go.set);
+		
+		
+		
 		System.out.println(go.answer);
 	}
-	void dfs(int i,String s,int length, List<String> num) {
+	
+	
+	
+	
+	
+	
+	void dfs(String s,int length, List<String> num,boolean[] check) {
 		
-		if(i==num.size()) {
-			System.err.println("set에 삽입됨 : "+ s);
+		if(s.length()==length) {
 			set.add(s);
-			
 			return;
 		}
-		if(s.length()>length||i<0||i>num.size()) {
-			return;
+		
+		for(int next = 0;next<num.size();next++) {
+			dfs(s+num.remove(next),length,num,check);
 		}
-//		System.out.println("현재 상황은?" + s);
-			dfs(i+1,s+num.get(i),length,num);
-			dfs(i-1,s+num.get(i),length,num);
 	}
 	
 	
--- src/p99programLv2/home100801할인행사.java
+++ src/p99programLv2/home100801할인행사.java
@@ -11,33 +11,37 @@ public class home100801할인행사 {
 		home100801할인행사 go= new home100801할인행사();
 		System.out.println(go.solution(new String[] {"banana", "apple", "rice", "pork", "pot"},
 				new int[] {3, 2, 2, 2, 1},
-				new String[] {"chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"}));
+				new String[] {"apple", "apple", "banana", "rice","chicken", "apple", "apple", "banana", "rice", "apple", "chicken","pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana","apple", "apple", "banana", "rice", "apple"}));
 	
 	}
 	
 	public int solution(String[] want, int[] number, String[] discount) {
-        Map<String,Integer> needs = new HashMap<>();
+		Map<String,Integer> needs = new HashMap<>();
+        Map<String,Integer> need = new HashMap<>();
         int answer=0;
         for(int i = 0 ; i < want.length;i++) {
         	needs.put(want[i], number[i]);
+        	need.put(want[i], number[i]);
         }
         Queue<String> shop = new LinkedList<>();
         
         for(int i = 0 ; i < discount.length;i++) {
         	if(!needs.containsKey(discount[i])) {
-        		answer=i+1;
         		shop.clear();
-        		System.err.println("원하는목록 아님 패스");
+                needs= new HashMap<>(need);
+                
+                System.err.println("원하는목록 아님 패스 needs : " + needs);
         		continue;
         	} else {
         		shop.add(discount[i]);
        			needs.put(discount[i], needs.get(discount[i])-1);
         	}
         	if(shop.size()>10) {
-        		needs.put(shop.peek(), needs.get(shop.poll())+1);
-        		answer++;
+        		needs.put(shop.peek(), needs.get(shop.peek())+1);
+        		shop.remove();
         		System.err.println("10개넘어서 뺌");
         	}
+        	
         	System.out.println(shop);
         	System.out.println(needs);
         	Set<String> menu = needs.keySet();
@@ -46,19 +50,12 @@ public class home100801할인행사 {
         		if(needs.get(goods)==0) {
         			cnt++;
         		}
-        		if(cnt==want.length) {
-        			System.out.println(i);
-        			answer+=1;
-        			return answer;
-        		}
+        	}
+        	if(cnt==want.length) {
+        		answer++;
         	}
         }
-        Set<String> menu = needs.keySet();
-        for(String goods : menu) {
-    		if(needs.get(goods)!=0) {
-    			return 0;
-    		}
-        }
+        
         return answer;
 	}
 }
