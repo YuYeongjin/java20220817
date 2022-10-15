285b100330b849acd6c4841b027996b285163c1b
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2022-10-15 21:35:52 +0900

    20221015프로그래머스 공부

--- src/p99programLv2/academy100602메뉴리뉴얼.java
+++ src/p99programLv2/academy100602메뉴리뉴얼.java
@@ -9,22 +9,134 @@ import java.util.Map;
 import java.util.Set;
 
 public class academy100602메뉴리뉴얼 {
-	String[] answer = {};
 	Set<String> food = new HashSet<>();
 	Map<String,Integer> menu = new HashMap<>();
+	Map<Character,Integer> order = new HashMap<>();
+	List<String> set = new ArrayList<>();
+	List<String> ans = new ArrayList<>();
 	public static void main(String[] args) {
 		academy100602메뉴리뉴얼 go = new academy100602메뉴리뉴얼();
 		
-//		String[] orders = {"ABCFG", "WQEWQEWQC", "CEQWEWQDE", "WEQRWQACDE", "BCFG", "ACWQEEDEH"};
-		String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
-		int[] course = {3,4,5};
-		
-		go.solution(orders, course);
-		System.out.println(Arrays.toString(go.answer));
+		String[] orders = {"ABCFG", "WQEWQEWQC", "CEQWEWQDE", "WEQRWQACDE", "BCFG", "ACWQEEDEH"};
+//		String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+		int[] course = {2,3,5};
+		System.out.println(go.solution(orders, course));
+
+	}
+
+	
+	public List<String> solution(String[] orders, int[] course) {
+		char[] ch;
+		for(int i = 0 ; i < orders.length;i++) {
+			if(orders[i].length()<course[0]) continue;
+			ch = orders[i].toCharArray();
+			for(int l = 0 ; l <ch.length;l++) {
+				if(!order.containsKey(ch[l])) {
+    				order.put(ch[l], 1);
+    			} else {
+    				order.put(ch[l], order.get(ch[l])+1);
+    			}
+			}
+		}
+		for(int i = 0 ; i < orders.length;i++) {
+			if(orders[i].length()<course[0]) continue;
+			String x = "";
+			for(int l = 0 ; l <orders[i].length();l++) {
+				if(order.get(orders[i].charAt(l))!=1) {
+					x+=orders[i].substring(l, l+1);
+				}
+			}
+			orders[i]=x;
+		}
+//		System.out.println(Arrays.toString(orders));
+        for(int i = 0 ; i < orders.length;i++) {
+        	food.clear();
+        	if(orders[i].length()>=course[0]) {
+        		int max = orders[i].length();
+        		dfs("",orders[i],course[0],course[course.length-1]);
+        	}
+        }
+        
+        food=menu.keySet();
+//        System.out.println(menu);
+        int[] max = new int[course.length];
+        for(String k : food) {
+        	if(menu.get(k)==1)continue;
+        	for(int i = 0 ; i < course.length;i++) {
+        		if(k.length()==course[i]&&menu.get(k)>=max[i]) {
+        			set.add(k);
+        			max[i]=menu.get(k);
+        			break;
+        		}
+        	}
+        }
+        
+        for(String k : set) {
+        	for(int i = 0 ; i < course.length;i++) {
+        		if(k.length()==course[i]&&menu.get(k)==max[i]) {
+        			ans.add(k);
+        			break;
+        		}
+        	}
+        }
+        ans.sort((a,b)->a.compareTo(b));
+        return ans;
 	}
 	
-	public String[] solution(String[] orders, int[] course) {
-		System.err.println(System.currentTimeMillis());
+	void dfs(String a, String b,int min,int max) {
+		if(a.length()>=min&&a.length()<=max) {
+			System.out.println(a);
+			char[] charA = a.toCharArray();
+			Arrays.sort(charA);
+			a=new String(charA);
+			if(food.add(a)) {
+    			if(!menu.containsKey(a)) {
+    				menu.put(a, 1);
+    			} else {
+    				menu.put(a, menu.get(a)+1);
+    			}
+    		}
+			if(a.length()>=max)
+			return;
+		}
+		for(int i = 0 ; i < b.length();i++) {
+			dfs(a+b.substring(i, i+1),b.substring(0, i)+b.substring(i+1),min,max);
+		}
+	}
+	
+
+}
+
+
+
+
+
+/*
+class Solution {
+    Set<String> food = new HashSet<>();
+    Map<String,Integer> menu = new HashMap<>();
+    Map<Character,Integer> order = new HashMap<>();
+    public List<String> solution(String[] orders, int[] course) {
+        char[] ch;
+		for(int i = 0 ; i < orders.length;i++) {
+			ch = orders[i].toCharArray();
+			for(int l = 0 ; l <ch.length;l++) {
+				if(!order.containsKey(ch[l])) {
+    				order.put(ch[l], 1);
+    			} else {
+    				order.put(ch[l], order.get(ch[l])+1);
+    			}
+			}
+		}
+		for(int i = 0 ; i < orders.length;i++) {
+			String x = "";
+			for(int l = 0 ; l <orders[i].length();l++) {
+				if(order.get(orders[i].charAt(l))!=1) {
+					x+=orders[i].substring(l, l+1);
+				}
+			}
+			orders[i]=x;
+		}
         for(int i = 0 ; i < orders.length;i++) {
         	food.clear();
         	for(int k = 0 ; k < course.length;k++) {
@@ -33,14 +145,12 @@ public class academy100602메뉴리뉴얼 {
         		}
         	}
         }
-        System.err.println(System.currentTimeMillis());
         food=menu.keySet();
-        System.out.println(menu);
         int[] max = new int[course.length];
         for(String k : food) {
         	if(menu.get(k)==1)continue;
         	for(int i = 0 ; i < course.length;i++) {
-        		if(menu.get(k)>=max[i]&&k.length()==course[i]) {
+        		if(k.length()==course[i]&&menu.get(k)>=max[i]) {
         			max[i]=menu.get(k);
         			break;
         		}
@@ -57,15 +167,11 @@ public class academy100602메뉴리뉴얼 {
         	}
         }
         ans.sort((a,b)->a.compareTo(b));
-        answer=new String[ans.size()];
-        for(int i = 0 ; i < ans.size();i++) {
-        	answer[i]=ans.get(i);
-        }
-        return answer;
+        return ans;
 	}
 	
 	void dfs(String a, String b,int c) {
-		if(!a.equals("")&&a.length()==c) {
+		if(a.length()==c) {
 			char[] charA = a.toCharArray();
 			Arrays.sort(charA);
 			a=new String(charA);
@@ -78,8 +184,12 @@ public class academy100602메뉴리뉴얼 {
     		}
 			return;
 		}
+		if(a.length()>c) {
+			return;
+		}
 		for(int i = 0 ; i < b.length();i++) {
 			dfs(a+b.substring(i, i+1),b.substring(0, i)+b.substring(i+1),c);
 		}
 	}
 }
+*/
