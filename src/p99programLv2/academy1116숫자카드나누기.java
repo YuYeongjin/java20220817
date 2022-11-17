00f3d0709ac03aefbe57012c06b610ddeee46fe7
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2022-11-17 17:19:25 +0900

    20221117 숫자카드나누기

--- src/p99programLv2/academy1116숫자카드나누기.java
+++ src/p99programLv2/academy1116숫자카드나누기.java
@@ -1,31 +1,51 @@
 package p99programLv2;
 
-import java.util.TreeSet;
+import java.util.*;
 
-public class academy1116숫자카드나누기{
-    TreeSet<Integer> ans = new TreeSet<>((a,b) ->b-a);
-    int answer = 0;
+public class academy1116숫자카드나누기 {
+    List<Integer> ans = new ArrayList<>();
+    Queue<Integer> firstAnswer = new LinkedList<>();
+
     public static void main(String[] args) {
 
-        academy1116숫자카드나누기 go  = new academy1116숫자카드나누기();
-        int[] arrayA = {14,35,119};
-        int[] arrayB = {18,30,102};
+        academy1116숫자카드나누기 go = new academy1116숫자카드나누기();
+        int[] arrayA = {10, 20};
+        int[] arrayB = {5,17};
+
+//        int[] arrayA = {14, 35, 119};
+//        int[] arrayB = {18, 30, 102};
         System.out.println(go.solution(arrayA, arrayB));
     }
-
     public int solution(int[] arrayA, int[] arrayB) {
-        ans.add(cheolsoo(arrayA, arrayB));
-        ans.add(younghee(arrayA, arrayB));
-        return answer;
-    }
-    int cheolsoo(int[] arrayA, int[] arrayB) {
-        int a= 0;
+        int min = Math.max(Arrays.stream(arrayB).min().getAsInt(),Arrays.stream(arrayA).min().getAsInt());
 
-        return a;
-    }
-    int younghee(int[] arrayA, int[] arrayB) {
-        int a= 0;
-
-        return a;
+        while(min!=1){
+            System.out.println("현재 : " + min);
+            int cntA =0; int cntB=0; int cntC=0; int cntD=0;
+            for(int i = 0;i<arrayA.length;i++){
+                if(arrayA[i]%min==0){
+                    cntA++;
+                }
+                if(arrayB[i]%min!=0){
+                    cntB++;
+                }
+                if(arrayA[i]%min != 0){
+                    cntC++;
+                }
+                if(arrayB[i]%min== 0) {
+                    cntD++;
+                }
+                if((cntA!=cntB) && (cntC!=cntD)){
+                    System.out.println("현재 : " +cntA+"+" +cntB+"+"+cntC+"+"+cntD);
+                    break;
+                }
+            }
+            if((cntA==cntB&&cntA==arrayA.length)||(cntC==cntD&&cntC==arrayB.length)){
+                ans.add(min);
+                break;
+            }
+            min--;
+        }
+        return ans.size()==0? 0: ans.size()==1? ans.get(0): ans.get(0)>ans.get(1)? ans.get(0): ans.get(1);
     }
 }
